7a27bd8b04f0a6e31ca6dbc7f0ec9550ccdc92a5
Wellysson Valadares Avelar - Rethink <wellysson@rethink.dev>
2020-10-10 11:41:54 -0300

    Correção projeto storyboarding

---
+++ hotmart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hotmart
+//
+//  Created by Wellysson Avelar on 10/10/20.
+//  Copyright © 2020 Wellysson Avelar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
